51838a36fd7677de4dcd6e3658126f1ec4239085
malul29 <161543349+malul29@users.noreply.github.com>
2024-11-21 16:27:28 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,11 +1,13 @@
 <?php
-include 'db.php';
-
+// include database connection file
+include_once("config.php");
+ 
+// Get id from URL to delete that user
 $id = $_GET['id'];
-
-$stmt = $conn->prepare("DELETE FROM users WHERE id = :id");
-$stmt->execute(['id' => $id]);
-
-header("Location: index.php");
-exit();
+ 
+// Delete user row from table based on given id
+$result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");
+ 
+// After delete redirect to Home, so that latest user list will be displayed.
+header("Location:index.php");
 ?>
